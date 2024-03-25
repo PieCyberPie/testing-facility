b6375f79074c9cdffde513a80d16e89664da4a46
PieCyberPie <piecyberpie@gmail.com>
2024-03-25 13:27:00 +0200

    isNan bs

---
+++ 1/script.js
@@ -0,0 +1,16 @@
+// if (parseInt('abd') !== NaN) {
+//     console.log('NaN');
+// }
+// console.log(parseInt('abd') + (' log'));
+
+console.log(parseInt('123456'));
+console.log(isNaN(parseInt('123456')));
+
+console.log(parseInt('abcdef'));
+console.log(isNaN(parseInt('abcdef')));
+
+console.log(parseInt('123def'));
+console.log(isNaN(parseInt('123def')));
+
+console.log(parseInt('abc456'));
+console.log(isNaN(parseInt('abc456')));
